5e23d2ec07276aff596680a4c3374a1f28fec1bb
ikarimullin <ikarimullin@i-novus.ru>
2023-08-03 11:18:16 +0300

    1.5 Intellij version 232.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.texhnolyzze"
-version = "1.4"
+version = "1.5"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("221")
-        untilBuild.set("231.*")
+        untilBuild.set("232.*")
     }
 
     signPlugin {
